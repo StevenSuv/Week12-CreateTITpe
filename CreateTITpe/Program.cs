42e3fda99476006335d8ee81804374358269e239
Opilane TTHK <opilane@tthk.ee>
2021-11-17 12:53:16 +0200

    Excersises

---
+++ CreateTITpe/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace CreateTITpe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\opilane\samples";
+            string userDirectory = "titpe21";
+            string fullDirectoryPath = $@"{rootDirectory}\{userDirectory}";
+
+            bool directoryExists = Directory.Exists(fullDirectoryPath);
+
+            if (directoryExists)
+            {
+                Console.WriteLine($"{userDirectory} already exists in {rootDirectory}.");
+            }
+            else if (!directoryExists)
+            {
+                Console.WriteLine($"{userDirectory} does not exist in {rootDirectory}.");
+                Directory.CreateDirectory(fullDirectoryPath);
+                Console.WriteLine($"{userDirectory} has been created in {rootDirectory}.");
+
+            }
+            else
+            {
+                Directory.CreateDirectory(fullDirectoryPath);
+                Console.WriteLine($"{userDirectory} has been created in {rootDirectory}.");
+            }
+        }
+    }
+}
---
+++ MyFiles/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace MyFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\opilane\samples\MyFiles.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            for (int i = 0; i < dataFromFile.Length; i++)
+            {
+                dataFromFile[i] = dataFromFile[i].Replace('4', 'a');
+                dataFromFile[i] = dataFromFile[i].Replace('3', 'e');
+                dataFromFile[i] = dataFromFile[i].Replace('0', 'o');
+                dataFromFile[i] = dataFromFile[i].Replace('1', 'i');
+            }
+
+            foreach (string line in dataFromFile)
+            {
+                Console.WriteLine(line);
+            }
+            for(int i = 0; i < dataFromFile.Length; i++)
+            {
+                File.Create($@"C:\Users\opilane\samples\titpe21\{dataFromFile[i]}.txt");
+            }
+
+            File.WriteAllLines(filePath, dataFromFile);
+        }
+    }
+}
